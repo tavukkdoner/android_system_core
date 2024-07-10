79e048a7d568d130805643216c9b39982ead7a20
chiteroman <chiteroman@users.noreply.github.com>
2024-07-10 01:49:22 -0400

    init: Spoof additional props
    Change-Id: I3a22f2a923628c54462ed878f2c98a228d355f60
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1427,6 +1427,8 @@ static void SetSafetyNetProps() {
     InitPropertySet("ro.boot.warranty_bit", "0");
     InitPropertySet("ro.warranty_bit", "0");
     InitPropertySet("ro.debuggable", "0");
+    InitPropertySet("ro.force.debuggable", "0");
+    InitPropertySet("ro.adb.secure", "1");
     InitPropertySet("ro.secure", "1");
     InitPropertySet("ro.bootimage.build.type", "user");
     InitPropertySet("ro.build.type", "user");
@@ -1444,6 +1446,7 @@ static void SetSafetyNetProps() {
     InitPropertySet("vendor.boot.vbmeta.device_state", "locked");
     InitPropertySet("vendor.boot.verifiedbootstate", "green");
     InitPropertySet("oplusboot.verifiedbootstate", "green");
+    InitPropertySet("sys.oem_unlock_allowed", "0");
 }
 
 void PropertyInit() {
