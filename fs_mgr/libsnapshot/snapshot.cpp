8f004ba8d56f7545614e976930f66eeadbf5a930
Nolen Johnson <johnsonnolen@gmail.com>
2025-12-01 23:46:53 -0500

    Revert "Remove support for separate A and B super partitions."
    This reverts commit 3934dc9f3fa7f9ff67479b85825355f3e014a60e.
    
    Reason: Several 4.14 and 4.19 shipping devices use this.
    Change-Id: I17dd51396035723323f5d4ba09967efdc84f5afb

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -765,7 +765,7 @@ bool SnapshotManager::MapSourceDevice(LockedFile* lock, const std::string& name,
     auto slot = SlotNumberForSlotSuffix(slot_suffix);
 
     CreateLogicalPartitionParams params = {
-            .block_device = device_->GetSuperDevice(),
+            .block_device = device_->GetSuperDevice(slot),
             .metadata = metadata,
             .partition_name = old_name,
             .timeout_ms = timeout_ms,
@@ -1685,7 +1685,7 @@ bool SnapshotManager::CollapseSnapshotDevice(LockedFile* lock, const std::string
     uint32_t slot = SlotNumberForSlotSuffix(device_->GetSlotSuffix());
     // Create a DmTable that is identical to the base device.
     CreateLogicalPartitionParams base_device_params{
-            .block_device = device_->GetSuperDevice(),
+            .block_device = device_->GetSuperDevice(slot),
             .metadata_slot = slot,
             .partition_name = name,
             .partition_opener = &device_->GetPartitionOpener(),
@@ -1950,7 +1950,7 @@ bool SnapshotManager::PerformInitTransition(InitTransition transition,
 std::unique_ptr<LpMetadata> SnapshotManager::ReadCurrentMetadata() {
     const auto& opener = device_->GetPartitionOpener();
     uint32_t slot = SlotNumberForSlotSuffix(device_->GetSlotSuffix());
-    auto super_device = device_->GetSuperDevice();
+    auto super_device = device_->GetSuperDevice(slot);
     auto metadata = android::fs_mgr::ReadMetadata(opener, super_device, slot);
     if (!metadata) {
         LOG(ERROR) << "Could not read dynamic partition metadata for device: " << super_device;
@@ -2019,7 +2019,7 @@ bool SnapshotManager::GetSnapshotFlashingStatus(LockedFile* lock,
     // metadata are in sync, so flashing all partitions on the source slot will
     // remove the UPDATED flag on the target slot as well.
     const auto& opener = device_->GetPartitionOpener();
-    auto super_device = device_->GetSuperDevice();
+    auto super_device = device_->GetSuperDevice(target_slot);
     auto metadata = android::fs_mgr::ReadMetadata(opener, super_device, target_slot);
     if (!metadata) {
         return false;
@@ -3033,7 +3033,7 @@ bool SnapshotManager::MapAllSnapshots(const std::chrono::milliseconds& timeout_m
     const auto& opener = device_->GetPartitionOpener();
     auto slot_suffix = device_->GetOtherSlotSuffix();
     auto slot_number = SlotNumberForSlotSuffix(slot_suffix);
-    auto super_device = device_->GetSuperDevice();
+    auto super_device = device_->GetSuperDevice(slot_number);
     auto metadata = android::fs_mgr::ReadMetadata(opener, super_device, slot_number);
     if (!metadata) {
         LOG(ERROR) << "MapAllSnapshots could not read dynamic partition metadata for device: "
@@ -3472,7 +3472,7 @@ Return SnapshotManager::CreateUpdateSnapshots(const DeltaArchiveManifest& manife
     uint32_t current_slot = SlotNumberForSlotSuffix(current_suffix);
     auto target_suffix = device_->GetOtherSlotSuffix();
     uint32_t target_slot = SlotNumberForSlotSuffix(target_suffix);
-    auto current_super = device_->GetSuperDevice();
+    auto current_super = device_->GetSuperDevice(current_slot);
 
     auto current_metadata = MetadataBuilder::New(opener, current_super, current_slot);
     if (current_metadata == nullptr) {
@@ -3611,8 +3611,8 @@ Return SnapshotManager::CreateUpdateSnapshots(const DeltaArchiveManifest& manife
                                     all_snapshot_status);
     if (!ret.is_ok()) return ret;
 
-    if (!UpdatePartitionTable(opener, device_->GetSuperDevice(), *exported_target_metadata,
-                              target_slot)) {
+    if (!UpdatePartitionTable(opener, device_->GetSuperDevice(target_slot),
+                              *exported_target_metadata, target_slot)) {
         LOG(ERROR) << "Cannot write target metadata";
         return Return::Error();
     }
@@ -4061,7 +4061,7 @@ bool SnapshotManager::UnmapAllPartitionsInRecovery() {
 
     const auto& opener = device_->GetPartitionOpener();
     uint32_t slot = SlotNumberForSlotSuffix(device_->GetSlotSuffix());
-    auto super_device = device_->GetSuperDevice();
+    auto super_device = device_->GetSuperDevice(slot);
     auto metadata = android::fs_mgr::ReadMetadata(opener, super_device, slot);
     if (!metadata) {
         LOG(ERROR) << "Could not read dynamic partition metadata for device: " << super_device;
@@ -4252,7 +4252,7 @@ bool SnapshotManager::HandleImminentDataWipe(const std::function<void()>& callba
 
     if (try_merge) {
         auto slot_number = SlotNumberForSlotSuffix(device_->GetSlotSuffix());
-        auto super_path = device_->GetSuperDevice();
+        auto super_path = device_->GetSuperDevice(slot_number);
         if (!CreateLogicalAndSnapshotPartitions(super_path, 20s)) {
             LOG(ERROR) << "Unable to map partitions to complete merge.";
             return false;
@@ -4300,7 +4300,7 @@ bool SnapshotManager::FinishMergeInRecovery() {
     }
 
     auto slot_number = SlotNumberForSlotSuffix(device_->GetSlotSuffix());
-    auto super_path = device_->GetSuperDevice();
+    auto super_path = device_->GetSuperDevice(slot_number);
     if (!CreateLogicalAndSnapshotPartitions(super_path, 20s)) {
         LOG(ERROR) << "Unable to map partitions to complete merge.";
         return false;
@@ -4431,7 +4431,7 @@ CreateResult SnapshotManager::RecoveryCreateSnapshotDevices(
 
     auto slot_suffix = device_->GetOtherSlotSuffix();
     auto slot_number = SlotNumberForSlotSuffix(slot_suffix);
-    auto super_path = device_->GetSuperDevice();
+    auto super_path = device_->GetSuperDevice(slot_number);
     if (!CreateLogicalAndSnapshotPartitions(super_path, 20s)) {
         LOG(ERROR) << "Unable to map partitions.";
         return CreateResult::ERROR;
